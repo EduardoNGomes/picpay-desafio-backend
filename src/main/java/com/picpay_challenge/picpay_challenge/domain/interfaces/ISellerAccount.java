828398e8e637dc7f11d633d9362f846a71148b18
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-02-27 18:51:55 -0300

    feat: implement interface ISellerAccount & IUserAccount

---
+++ src/main/java/com/picpay_challenge/picpay_challenge/domain/interfaces/ISellerAccount.java
@@ -0,0 +1,5 @@
+package com.picpay_challenge.picpay_challenge.domain.interfaces;
+
+public interface ISellerAccount {
+  public void increaseBalance(Double value);
+}
---
+++ src/main/java/com/picpay_challenge/picpay_challenge/domain/interfaces/IUserAccount.java
@@ -0,0 +1,7 @@
+package com.picpay_challenge.picpay_challenge.domain.interfaces;
+
+public interface IUserAccount {
+  public void increaseBalance(Double value);
+
+  public void decreaseBalance(Double value);
+}
