3363804d22a7da091c27fd654817b53613ec56af
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-02-27 09:49:50 -0300

    test: implement composer to run database

---
+++ src/main/java/com/picpay_challenge/picpay_challenge/PicpayChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.picpay_challenge.picpay_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpay_challenge/picpay_challenge/PicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay_challenge.picpay_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
