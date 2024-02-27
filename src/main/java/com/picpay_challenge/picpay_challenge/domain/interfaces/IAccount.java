75649f460b01f5fd63f4a5924f03ced27925609c
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-02-27 17:33:19 -0300

    feat: implement IAccount

---
+++ src/main/java/com/picpay_challenge/picpay_challenge/domain/interfaces/IAccount.java
@@ -0,0 +1,8 @@
+package com.picpay_challenge.picpay_challenge.domain.interfaces;
+
+import com.picpay_challenge.picpay_challenge.core.entities.UniqueEntityID;
+
+public class IAccount {
+  public Double balance;
+  public UniqueEntityID ownerID;
+}
