168dbfa50fded19b827ca90d607685e0581352bb
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-02-28 16:12:49 -0300

    feat: implement new method on  UserRepository

--- src/main/java/com/picpay_challenge/picpay_challenge/infra/database/repositories/UserRepository.java
+++ src/main/java/com/picpay_challenge/picpay_challenge/infra/database/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package com.picpay_challenge.picpay_challenge.infra.database.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.picpay_challenge.picpay_challenge.infra.database.entities.UserEntityToRepository;
+import com.picpay_challenge.picpay_challenge.infra.database.entities.UserEntityDatabase;
 
-public interface UserRepository extends JpaRepository<UserEntityToRepository, UUID> {
+public interface UserRepository extends JpaRepository<UserEntityDatabase, UUID> {
 
+  public Optional<UserEntityDatabase> findByEmailOrCpf(String email, String cpf);
 }
