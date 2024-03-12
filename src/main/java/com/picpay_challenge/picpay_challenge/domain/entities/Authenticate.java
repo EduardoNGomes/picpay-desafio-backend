9795d7d26428fed63b3e3339fe57922614cfa966
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-03-12 13:48:06 -0300

    feat: implement Autthenticate entity

---
+++ src/main/java/com/picpay_challenge/picpay_challenge/domain/entities/Authenticate.java
@@ -0,0 +1,26 @@
+package com.picpay_challenge.picpay_challenge.domain.entities;
+
+import com.picpay_challenge.picpay_challenge.core.vo.UniqueEmail;
+import com.picpay_challenge.picpay_challenge.domain.enums.Roles;
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@Builder
+public class Authenticate {
+
+	private final UniqueEmail email;
+	private final String password;
+	private final Roles role;
+	private final String id;
+
+	public Authenticate(UniqueEmail email, String password, Roles role, String id) {
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.id = id;
+	}
+
+
+}
