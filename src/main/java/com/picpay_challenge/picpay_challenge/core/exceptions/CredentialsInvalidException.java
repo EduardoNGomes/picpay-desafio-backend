c5486381d249cb392f5645619c3ccd0a39ebd050
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-03-12 13:49:30 -0300

    feat: implement Credentials Invalid exceptions

---
+++ src/main/java/com/picpay_challenge/picpay_challenge/core/exceptions/CredentialsInvalidException.java
@@ -0,0 +1,9 @@
+package com.picpay_challenge.picpay_challenge.core.exceptions;
+
+public class CredentialsInvalidException extends RuntimeException {
+
+	public CredentialsInvalidException() {
+		super("Credentials invalid");
+	}
+
+}
