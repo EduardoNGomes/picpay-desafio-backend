c770a9ba2cbb5ce6755a940ee3eea5734cc63711
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-03-15 17:05:26 -0300

    feat: implement TransferUseCase

---
+++ src/main/java/com/picpay_challenge/picpay_challenge/domain/useCases/Transfer/TransferUseCase.java
@@ -0,0 +1,75 @@
+package com.picpay_challenge.picpay_challenge.domain.useCases.Transfer;
+
+import com.picpay_challenge.picpay_challenge.core.entities.UniqueEntityID;
+import com.picpay_challenge.picpay_challenge.core.exceptions.InsufficientBalanceException;
+import com.picpay_challenge.picpay_challenge.core.exceptions.PermissionDeniedException;
+import com.picpay_challenge.picpay_challenge.core.exceptions.ResourceNotFound;
+import com.picpay_challenge.picpay_challenge.core.repositories.AccountRepository;
+import com.picpay_challenge.picpay_challenge.core.validation.ApproveTransfer;
+import com.picpay_challenge.picpay_challenge.domain.enums.Roles;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransferUseCase {
+
+	private final AccountRepository accountRepository;
+
+	private final ApproveTransfer approveTransfer;
+
+	@Autowired
+	public TransferUseCase(AccountRepository accountRepository, ApproveTransfer approveTransfer) {
+		this.accountRepository = accountRepository;
+		this.approveTransfer = approveTransfer;
+	}
+
+
+	public void execute(String userIdFrom, String roleUserFrom, Double value, String userIdTo) throws
+			PermissionDeniedException, ResourceNotFound,
+			InsufficientBalanceException {
+
+		if (Roles.SELLER.getState().equals(roleUserFrom)) {
+			throw new PermissionDeniedException("Seller account can not transfer money");
+		}
+
+		if (!roleUserFrom.equals("USER")) {
+			throw new PermissionDeniedException("Account type is invalid");
+		}
+
+		var ownerAccountFromId = new UniqueEntityID(Optional.of(UUID.fromString(userIdFrom)));
+
+		var ownerAccountFrom = accountRepository.findByOwnerId(ownerAccountFromId);
+
+		var ownerAccountToId = new UniqueEntityID(Optional.of(UUID.fromString(userIdTo)));
+
+		var ownerAccountTo = accountRepository.findByOwnerId(ownerAccountToId);
+
+		if (ownerAccountFrom.isEmpty() | ownerAccountTo.isEmpty()) {
+			throw new ResourceNotFound();
+		}
+
+		if (ownerAccountFrom.get().getBalance() < value) {
+			throw new InsufficientBalanceException();
+		}
+
+		var approvedTransaction = approveTransfer.confirm();
+
+		if (!approvedTransaction) {
+			throw new PermissionDeniedException("");
+		}
+
+		var accountIdToDecrease = new UniqueEntityID(Optional.ofNullable(ownerAccountFrom.get()
+				.getIdValue()));
+
+		accountRepository.decreaseBalance(value, accountIdToDecrease);
+
+		var accountIdToIncrease = new UniqueEntityID(Optional.ofNullable(ownerAccountTo.get()
+				.getIdValue()));
+
+		accountRepository.increaseBalance(value, accountIdToIncrease);
+
+	}
+
+}
